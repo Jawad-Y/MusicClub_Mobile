48db95205f997da1abfd48586030806b37d94647
majed adam <mohammad.hm.836@gmail.com>
2026-01-22 17:07:36 +0200

    Merging all existing branches, adding a profile page, adding more to the bottom navigation, adding some icons, and general fixes.

--- app/src/main/java/com/music/musicclub/HostActivity.java
+++ app/src/main/java/com/music/musicclub/HostActivity.java
@@ -1,7 +1,10 @@
 package com.music.musicclub;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -9,21 +12,53 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class HostActivity extends AppCompatActivity {
+
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.host_layout);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
 
-        assert navHostFragment != null;
-        NavController navController = navHostFragment.getNavController();
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        NavigationUI.setupWithNavController(bottomNav, navController);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_more) {
+                    showBottomSheet();
+                    return false;
+                }
+                return NavigationUI.onNavDestinationSelected(item, navController);
+            }
+        });
+    }
+
+    private void showBottomSheet() {
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet_overflow, null);
+        bottomSheetDialog.setContentView(bottomSheetView);
+
+        NavigationView moreNavigationView = bottomSheetView.findViewById(R.id.more_navigation_view);
+
+        moreNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                bottomSheetDialog.dismiss();
+                return NavigationUI.onNavDestinationSelected(item, navController);
+            }
+        });
+
+        bottomSheetDialog.show();
     }
 }
